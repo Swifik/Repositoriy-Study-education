3419e5510d081f734fdf3e88ec2b2bd9e1516af8
Swifik <160834446+Swifik@users.noreply.github.com>
2024-02-22 14:10:12 +0500

    Add files via upload

---
+++ chepochka/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        ValidateTransaction firstValidation = new MinLimit();
+        ValidateTransaction secondValidation = new MaxLimit();
+        firstValidation.setNext(secondValidation);
+
+        firstValidation.check(new MoneyTransaction(12000));
+    }
+}
