5e74166a81ebe5888f8a1e1fda576051bc1f4a87
superspinerVADIMKA <160835503+superspinerVADIMKA@users.noreply.github.com>
2024-03-02 12:57:46 +0500

    Add files via upload

---
+++ untitled2 исправил внатуре чётко/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    static void Error()
+    {
+        int numberError = 10;
+        System.out.println("Привет");
+        System.out.println("Ошибка номер " + numberError + ". Проверьте подключение к Интернету");
+        System.out.println("Или обратитесь к провайдеру");
+    }
+    public static void main(String[] args) {
+        Error();
+    }
+}
